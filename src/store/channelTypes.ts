62f9d81bf449ce12efef02ae255e0e0c3775b1c3
Brock <brockreece@gmail.com>
2021-09-28 17:50:04 +0100

    Refactor loadMore logic into reusable function

--- src/store/channelTypes.ts
+++ src/store/channelTypes.ts
@@ -1,5 +1,5 @@
 import { createResource, createSignal, createMemo } from 'solid-js'
-import { PLATFORM_API_BASE, DjangoList } from './platformAPIBase'
+import { BASE_PAGINATION_LIMIT, PLATFORM_API_BASE, DjangoList } from './platformAPIBase'
 
 export interface ChannelType {
     id: string
@@ -8,9 +8,11 @@ export interface ChannelType {
 }
 
 export const [ search, setSearch ] = createSignal('')
+export const limit = BASE_PAGINATION_LIMIT
 
 const autoRefreshParams = createMemo(() => ({
-    search: search()
+    search: search(),
+    limit
 }))
 
 export const channelTypeResource = createResource<DjangoList<ChannelType>, typeof autoRefreshParams>(autoRefreshParams, (autoRefreshParams) => {
@@ -21,18 +23,3 @@ export const channelTypeResource = createResource<DjangoList<ChannelType>, typeo
     return fetch(`${PLATFORM_API_BASE}/channel-types?${params}`)
         .then(response => response.json())
 })
-
-export function loadMoreChannelTypes() {
-    const nextURL = new URL(channelTypeResource[0]().next)
-    return fetch(`${PLATFORM_API_BASE}/${nextURL.pathname}${nextURL.search}`)
-        .then(res => res.json())
-        .then((res) => {
-            channelTypeResource[1].mutate((prev) => {
-                res.results = [
-                    ...prev.results,
-                    ...res.results
-                ]
-                return res
-            })
-        })
-}
--- src/store/platformAPIBase.ts
+++ src/store/platformAPIBase.ts
@@ -1,8 +1,45 @@
+import { createSignal, createMemo, ResourceReturn, Accessor } from "solid-js" 
 export const PLATFORM_API_BASE = '/api'
+
+export const BASE_PAGINATION_LIMIT = 10
 
 export interface DjangoList<T> {
     count: number
     next: string | null
     previous: string | null 
     results: T[]
+}
+
+
+export function useLoadMore<T>(resource: ResourceReturn<DjangoList<T>>, limit: Accessor<number> | number  = BASE_PAGINATION_LIMIT) {
+    const [ isLoadingMore, setIsLoadingMore ] = createSignal(false)
+
+    return [
+        {
+            isLoadingMore,
+            nextPageSize: createMemo(() => {
+                return Math.min(resource[0]()?.count - resource[0]()?.results.length, typeof limit === 'function' ? limit() : limit)
+            })
+        },
+        {
+            loadMore() {
+                setIsLoadingMore(true)
+                const nextURL = new URL(resource[0]().next)
+                return fetch(`${PLATFORM_API_BASE}/${nextURL.pathname}${nextURL.search}`)
+                    .then(res => res.json())
+                    .then((res) => {
+                        resource[1].mutate((prev) => {
+                            res.results = [
+                                ...prev.results,
+                                ...res.results
+                            ]
+                            return res
+                        })
+                    })
+                    .finally(() => {
+                        setIsLoadingMore(false)
+                    })
+            }
+        }
+    ]
 }

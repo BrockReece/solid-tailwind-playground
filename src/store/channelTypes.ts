2cb342398f1e56a265f8084cc713e657dc41b81d
Brock <brockreece@gmail.com>
2021-10-01 11:32:20 +0100

    Convert channelTypes params to store

--- src/store/channelTypes.ts
+++ src/store/channelTypes.ts
@@ -1,4 +1,5 @@
-import { createResource, createSignal, createMemo } from 'solid-js'
+import { createResource, createMemo } from 'solid-js'
+import { createStore } from "solid-js/store"
 import { BASE_PAGINATION_LIMIT, PLATFORM_API_BASE, DjangoList } from './platformAPIBase'
 
 export interface ChannelType {
@@ -7,17 +8,19 @@ export interface ChannelType {
     slug: string
 }
 
-export const [ search, setSearch ] = createSignal('')
-export const limit = BASE_PAGINATION_LIMIT
+export const [ filters, setFilters ] = createStore({
+    search: '',
+    limit: BASE_PAGINATION_LIMIT
+})
 
 const autoRefreshParams = createMemo(() => ({
-    search: search(),
-    limit
+    search: filters.search,
+    limit: filters.limit
 }))
 
-export const channelTypeResource = createResource<DjangoList<ChannelType>, typeof autoRefreshParams>(autoRefreshParams, (autoRefreshParams) => {
+export const channelTypeResource = createResource<DjangoList<ChannelType>, typeof autoRefreshParams>(autoRefreshParams, (_) => {
     const params = new URLSearchParams({
-        ...autoRefreshParams
+        ...(filters as Record<string, any>)
     })
     
     return fetch(`${PLATFORM_API_BASE}/channel-types?${params}`)
